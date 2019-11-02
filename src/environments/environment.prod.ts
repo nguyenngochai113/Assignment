dfc61ed285acf656de2384e58b83cbc422a19468
= <ngochai5750@gmail.com>
2019-11-02 01:43:44 -0500

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAG8V_icdvfUxn1YzGXPXCL4uP90aZPopg",
+    authDomain: "nguyenngochai-549cd.firebaseapp.com",
+    databaseURL: "https://nguyenngochai-549cd.firebaseio.com",
+    projectId: "nguyenngochai-549cd",
+    storageBucket: "nguyenngochai-549cd.appspot.com",
+    messagingSenderId: "661031881917",
+    appId: "1:661031881917:web:57196cf4ba72a301c5fbd4",
+    measurementId: "G-53RFJTYGWR"
+  }
 };
